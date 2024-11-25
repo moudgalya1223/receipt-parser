76f3b0a1bc2c542057e37bc07327d64273f02295
Asswanth Chaitanya Saradhi <88917196+codersaradhi@users.noreply.github.com>
2024-11-25 07:01:36 +0000

    update frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'receipt-parser';
+  hiddenComponents: any;
+
+  letGo() {
+    // Hiding both the button and the h1 by selecting their ids
+    const button = document.getElementById('main-btn');
+    const title = document.getElementById('main-title');
+    
+    if (button && title) {
+      button.style.display = 'none';
+      title.style.display = 'none';
+    }
+  }
 }
